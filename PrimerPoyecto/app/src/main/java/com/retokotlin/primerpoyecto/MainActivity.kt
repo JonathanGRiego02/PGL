a82e680ccf94f444e79a134783a052ad927b6f4f
unknown <jonathangrclases@gmail.com>
2024-10-01 09:33:07 +0100

    primer proyecto

---
+++ PrimerPoyecto/app/src/main/java/com/retokotlin/primerpoyecto/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.retokotlin.primerpoyecto
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.retokotlin.primerpoyecto.ui.theme.PrimerPoyectoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrimerPoyectoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val intent = Intent(this, Actividad2::class.java)
+        startActivity(intent)
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PrimerPoyectoTheme {
+        Greeting("Android")
+    }
+
+
+
+}
